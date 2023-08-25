a961563b812fa936f963641270d0d18a7aae7778
pappik <pappik@gmail.com>
2023-08-25 08:14:50 -0400

    nginx-roboshop config file

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,29 @@
-yum install nginx -y
+scriptlocation=$(pwd)
+LOG=/tmp/roboshop.log
 
+echo -e "\e[35m Install Nginx\e[0m"
+yum install nginx -y &>>${LOG}
+
+echo -e "\e[35m Remove Nginx Old content\e[0m"
+rm -rf /usr/share/nginx/html/* &>>${LOG}
+
+echo -e "\e[35m Download frontend content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+
+
+cd /usr/share/nginx/html &>>${LOG}
+
+echo -e "\e[35m Extracting frontend content\e[0m"
+unzip /tmp/frontend.zip &>>${LOG}
+
+echo -e "\e[35m Roboshop Nginx config file \e[0m"
+cp ${scriptlocation}/files/nginx-roboshop.conf /etc/nginx/default.d/robodhop.conf &>>{LOG}
+
+echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35m Start Nginx\e[0m"
 systemctl start nginx
-
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
 
 
