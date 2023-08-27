6294705d2c5cd06f16c72c62d3ffc5b1b430f94e
pappik <pappik@gmail.com>
2023-08-27 08:50:36 -0400

    added success message

--- frontend.sh
+++ frontend.sh
@@ -1,70 +1,43 @@
 scriptlocation=$(pwd)
 LOG=/tmp/roboshop.log
-
+status_check() {
+ if [ $? -eq 0 ]
+ then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi
+}
 echo -e "\e[35m Install Nginx\e[0m"
 yum install nginx -y &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
+
 echo -e "\e[35m Remove Nginx Old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 
 echo -e "\e[35m Download frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 
 
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[35m Extracting frontend content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 
 echo -e "\e[35m Roboshop Nginx config file \e[0m"
 cp ${scriptlocation}/files/nginx-roboshop.conf /etc/nginx/default.d/robodhop.conf &>>{LOG}
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 
 echo -e "\e[35m Enable Nginx\e[0m"
 systemctl enable nginx
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 
 echo -e "\e[35m Start Nginx\e[0m"
 systemctl start nginx
-if [ $? -eq 0 ]
-then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+status_check
 
 
 
