f9f53b27eec1909bf0e7c0db3d757faf7b9ab5af
pappik <pappik@gmail.com>
2023-08-28 15:36:28 -0400

    updated script dry code1

--- common.sh
+++ common.sh
@@ -3,8 +3,10 @@ LOG=/tmp/roboshop.log
 status_check() {
  if [ $? -eq 0 ]
  then
-   echo SUCCESS
+   echo -e "\e[1;32mSUCCESS\e[om"
  else
-   echo FAILURE
+   echo -e "\e[1;31mFAILURE\e[0m"
+   echo "Refer lof file for more information , LOG-$(LOG)"
+   exit
  fi
 }
