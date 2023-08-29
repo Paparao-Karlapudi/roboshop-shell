3f1ec96fab726c7db2831261fdeb8b73aba5ad86
pappik <pappik@gmail.com>
2023-08-29 11:37:59 -0400

    code dry

--- common.sh
+++ common.sh
@@ -14,3 +14,75 @@ status_check() {
 print_head(){
   echo -e "\e[35m$1\e[0m"
 }
+
+NODEJS(){
+  source common.sh
+
+  print_head "Installing Nodejs"
+  yum install nodejs -y &>>{LOG}
+  status_check
+
+
+  print_head "Adding ${component}"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]
+  then
+    useradd  roboshop
+  fi
+
+
+  print_head "Making directory"
+  mkdir -p /app
+
+
+  print_head "Downloading catalogue"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>{LOG}
+  rm -rf /app/*
+  status_check
+
+
+  cd /app
+
+  print_head "Extracting ${component}"
+  unzip /tmp/${component}.zip &>>{LOG}
+  status_check
+
+  cd /app
+
+  print_head "Installing dependencies"
+  npm install &>>{LOG}
+  status_check
+
+
+
+  print_head "Configuring ${component} service file"
+  cp ${scriptlocation}/files/${component}.service /etc/systemd/system/${component}.service &>>{LOG}
+  status_check
+
+
+
+  print_head "Enabling ${component}"
+  systemctl enable ${component} &>>{LOG}
+  status_check
+
+
+  print_head "Starting ${component}"
+  systemctl start ${component} &>>{LOG}
+  status_check
+
+
+  print_head "Copying mongod repo file"
+  cp ${scriptlocation}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>{LOG}
+  status_check
+
+
+  print_head Installing mongod client
+  yum install mongodb-org-shell -y &>>{LOG}
+  status_check
+
+
+  print_head Loading Schema
+  mongo --host mongodb-dev.pappik.online </app/schema/${component}.js &>>{LOG}
+  status_check
+
+}
