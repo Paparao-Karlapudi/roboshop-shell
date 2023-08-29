9bfa70ffb2970d06e705be86684f43eacaf4ed01
pappik <pappik@gmail.com>
2023-08-29 11:12:05 -0400

    update

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ LOG=/tmp/roboshop.log
 status_check() {
  if [ $? -eq 0 ]
  then
-   echo -e "\e[1;32mSUCCESS\e[om"
+   echo -e "\e[1;32mSUCCESS\e[0m"
  else
    echo -e "\e[1;31mFAILURE\e[0m"
    echo "Refer lof file for more information , LOG -${LOG}"
