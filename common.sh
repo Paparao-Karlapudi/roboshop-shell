b29d4de8968ce950ce953f2aee9befbca838451b
pappik <pappik@gmail.com>
2023-09-01 08:33:13 -0400

    shipping

--- common.sh
+++ common.sh
@@ -15,6 +15,84 @@ print_head(){
   echo -e "\e[35m$1\e[0m"
 }
 
+APP_PREREQ(){
+
+    print_head "Adding Application user"
+    id roboshop &>>${LOG}
+    if [ $? -ne 0 ]
+    then
+      useradd  roboshop
+    fi
+
+
+    print_head "Making directory"
+    mkdir -p /app
+
+
+    print_head "Downloading ${component}"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>{LOG}
+    rm -rf /app/*
+    status_check
+
+
+    cd /app
+
+    print_head "Extracting ${component}"
+    unzip /tmp/${component}.zip &>>{LOG}
+    status_check
+}
+
+SYSTEMD_SETUP(){
+    print_head "Configuring ${component} service file"
+    cp ${scriptlocation}/files/${component}.service /etc/systemd/system/${component}.service &>>{LOG}
+    status_check
+
+    print_head "Reloading the service"
+    systemctl daemon-reload &>>{LOG}
+    status_check
+
+    print_head "Enabling ${component}"
+    systemctl enable ${component} &>>{LOG}
+    status_check
+
+
+    print_head "Starting ${component}"
+    systemctl start ${component} &>>{LOG}
+    status_check
+  }
+
+LOAD_SCHEMA(){
+  if [ ${schema_load} == true ]; then
+   if [ ${schema_type} == mongo ]; then
+    print_head "Copying mongod repo file"
+    cp ${scriptlocation}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>{LOG}
+    status_check
+
+
+    print_head "Installing mongod client"
+    yum install mongodb-org-shell -y &>>{LOG}
+    status_check
+
+
+    print_head "Loading Schema"
+    mongo --host mongodb-dev.pappik.online </app/schema/${component}.js &>>{LOG}
+    status_check
+  fi
+   if [ ${schema_type} == mysql ]; then
+
+      print_head "Installing mysql client"
+      yum install mysql -y &>>{LOG}
+      status_check
+
+
+      print_head "Loading Schema"
+      mysql -h mysql-dev.pappik.online -uroot -p${root_mysql_password} </app/schema/shipping.sql &>>{LOG}
+      status_check
+    fi
+
+fi
+}
+
 NODEJS(){
 
   print_head "Configuring nodejs repos"
@@ -26,29 +104,7 @@ NODEJS(){
   status_check
 
 
-  print_head "Adding Application user"
-  id roboshop &>>${LOG}
-  if [ $? -ne 0 ]
-  then
-    useradd  roboshop
-  fi
-
-
-  print_head "Making directory"
-  mkdir -p /app
-
-
-  print_head "Downloading ${component}"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>{LOG}
-  rm -rf /app/*
-  status_check
-
-
-  cd /app
-
-  print_head "Extracting ${component}"
-  unzip /tmp/${component}.zip &>>{LOG}
-  status_check
+  APP_PREREQ
 
   cd /app
 
@@ -56,38 +112,30 @@ NODEJS(){
   npm install &>>{LOG}
   status_check
 
+  SYSTEMD_SETUP
 
+  LOAD_SCHEMA
+}
 
-  print_head "Configuring ${component} service file"
-  cp ${scriptlocation}/files/${component}.service /etc/systemd/system/${component}.service &>>{LOG}
+MAVEN(){
+  print_head "Intstall Maven"
+  yum install maven -y &>>${LOG}
   status_check
 
-  print_head "Reloading the service"
-  systemctl daemon-reload &>>{LOG}
+  APP_PREREQ
+
+  cd /app
+
+  print_head "build a package"
+  mvn clean package
   status_check
 
-  print_head "Enabling ${component}"
-  systemctl enable ${component} &>>{LOG}
+  print_head "copy app to App Location"
+  mv target/${component}-1.0.jar ${component}.jar
   status_check
 
-
-  print_head "Starting ${component}"
-  systemctl start ${component} &>>{LOG}
-  status_check
-
-if [ ${schema_load} == true ]; then
-  print_head "Copying mongod repo file"
-  cp ${scriptlocation}/files/mongo.repo /etc/yum.repos.d/mongo.repo &>>{LOG}
-  status_check
+  SYSTEMD_SETUP
 
 
-  print_head Installing mongod client
-  yum install mongodb-org-shell -y &>>{LOG}
-  status_check
 
-
-  print_head Loading Schema
-  mongo --host mongodb-dev.pappik.online </app/schema/${component}.js &>>{LOG}
-  status_check
-fi
 }
---
+++ shipping.sh
@@ -0,0 +1,13 @@
+source common.sh
+if[ -z "${root_mysql_password}" ];then
+  echo "varibale root_mysql_password is empty"
+  exit
+fi
+
+component=catalogue
+
+schema_load=true
+schema_type=mysql
+
+MAVEN
+
